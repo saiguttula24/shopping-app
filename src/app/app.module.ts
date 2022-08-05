6bd134427123a2acf7ee002194a1e06df90d5642
sai guttula <saiguttula24@gmail.com>
2022-08-05 14:04:32 +0530

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { NavComponent } from './home/nav/nav.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: '' ,component: HomeComponent},
+      {path: 'login', component: LoginComponent},
+      {path:'signup',component:SignupComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
